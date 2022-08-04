d8253a44ecb7f3b05bb6cf7348eca5f84164681f
Render <kasatkin_16@mail.ru>
2022-08-04 18:32:25 +0300

    matrix solved

--- table.py
+++ table.py
@@ -4,24 +4,33 @@
 a = [[9, 5, 3], [0, 7, -1],[-5, 2, 9], ['end']]
 del a[-1]
 print(a)
-# b = []
+c = []
+b = []
 # print(len(a))
 # print(len(a[0]))
-for i in range(0, len(a)-1):
-    for j in range(0, len(a[0])-1):
-        s = 0
-        for di in range(-1, 2):
-            for dj in range(-1, 2):
-                ai = i + di
-                aj = j + dj
-                if ai + aj - i - j == 1 or ai + aj - i - j == -1:
-                # print(ai, end=' ')
-                # print(aj, end=' ')
-                # print(s)
-                    s = s + int(a[ai][aj])
-                else:
-                    continue
+s = 0
+for i in range(0, len(a)):
+    for j in range(0, len(a[0])):
+        s = a[i-1][j] + a[(i+1)%(len(a))][j] + a[i][j-1] + a[i][(j+1)%(len(a[0]))]
+        c += [s]
         print(s)
+    print(c)
+    b += [[c]]
+    c = []
+
+print(b)
+
+        # for di in range(-1, len(a)-1):
+        #     for dj in range(-1, 2):
+        #         ai = i + di
+        #         aj = j + dj
+        #         if ai + aj - i - j == 1 or ai + aj - i - j == -1:
+        #         # print(ai, end=' ')
+        #         # print(aj, end=' ')
+        #         # print(s)
+        #             s = s + int(a[ai][aj])
+        #         else:
+        #             continue
 
 
 
