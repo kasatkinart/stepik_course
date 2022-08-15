68f133030ae6a89ec52e60566b6fe780a50deffd
Render <kasatkin_16@mail.ru>
2022-08-15 10:38:14 +0300

    war and peace solved

--- war and peace.py
+++ war and peace.py
@@ -1,2 +1,10 @@
-a = [int(i) for i in input().split()]
+a = [(str(i)).lower() for i in input().split()]
+d = {}
+for i, n in enumerate(a):
+    s = a.count(n)
+    d[n] = s
+
+for key, value in d.items():
+    print(key, value, end=' ')
+    print()
 
