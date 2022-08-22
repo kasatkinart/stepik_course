296a60cdc8145e053c4087bcaa546356db665712
Render <kasatkin_16@mail.ru>
2022-08-22 12:44:16 +0300

    footballTable.py slvd

--- footballTable.py
+++ footballTable.py
@@ -6,55 +6,36 @@ while games < n:
     a.append(input().split(';'))
     games += 1
 
-# print(games)
-# print(a)
-# print(a[0][0])
 d = {}
 
 for i in range(0, n):
-    for j in 0, 2:
-        d[a[i][j]] = [0, 0, 0, 0, 0]
+    for j in (0, 2):
+        if a[i][j] in d:
+            continue
+        elif a[i][j] not in d:
+            d[a[i][j]] = [0]*5
+    d[a[i][0]][0] += 1
+    d[a[i][2]][0] += 1
+    if int(a[i][1]) > int(a[i][3]):
+        d[a[i][0]][1] += 1
+        d[a[i][0]][4] += 3
+        d[a[i][2]][3] += 1
+    elif int(a[i][1]) == int(a[i][3]):
+        d[a[i][0]][2] += 1
+        d[a[i][2]][2] += 1
+        d[a[i][0]][4] += 1
+        d[a[i][2]][4] += 1
+    elif int(a[i][1]) < int(a[i][3]):
+        d[a[i][0]][3] += 1
+        d[a[i][2]][4] += 3
+        d[a[i][2]][1] += 1
 
-j = 0
-
-for i in range(0, n):
-        d[a[i][0]][0] += 1
-        d[a[i][2]][0] += 1
-        if a[i][1] < a[i][3]:
-            d[a[i][0]][1] += 1
-            d[a[i][0]][4] += 3
-            d[a[i][2]][3] += 1
-        elif a[i][1] == a[i][3]:
-            d[a[i][0]][2] += 1
-            d[a[i][2]][2] += 1
-            d[a[i][0]][4] += 1
-            d[a[i][2]][4] += 1
-        elif a[i][1] > a[i][3]:
-            d[a[i][2]][1] += 1
-            d[a[i][2]][4] += 3
-            d[a[i][0]][3] += 1
-
-
-# print(d.items())
 
 for q, w in d.items():
     print((q+':'), *w, end='\n')
 
 
-# tableResult[a[i][0]] = [0, 0, 0, 0, 0]
-# tableResult[a[i][2]] = [0, 0, 0, 0, 0]
-# tableResult[a[i][0]][0] += 1
-# tableResult[a[i][2]][0] += 1
-# if a[i][1] > a[i][3]:
-#     tableResult[a[i][0]][1] += 1
-#     tableResult[a[i][0]][4] += 3
-#     tableResult[a[i][2]][3] += 1
-# elif a[i][1] == a[i][3]:
-#     tableResult[a[i][0]][2] += 1
-#     tableResult[a[i][2]][2] += 1
-#     tableResult[a[i][0]][4] += 1
-#     tableResult[a[i][2]][4] += 1
-# elif a[i][1] < a[i][3]:
-#     tableResult[a[i][2]][1] += 1
-#     tableResult[a[i][2]][4] += 3
-#     tableResult[a[i][0]][3] += 1
+
+
+
+
